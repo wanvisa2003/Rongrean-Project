c8241aa5079f6940afaec0c58fe44bc74cbceb70
wansai <wannvisa4488@gmail.com>
2024-07-22 20:56:49 +0700

    add total student graduated

--- rongrean/app.py
+++ rongrean/app.py
@@ -1,104 +1,5 @@
 
-
-# import dash
-# from dash import dcc, html, Input, Output
-# import plotly.express as px
-# import pandas as pd
-# import requests
-# import json
-
-# # สร้างแอปพลิเคชัน Dash
-# app = dash.Dash(__name__)
-
-# # โหลดข้อมูล GeoJSON ของประเทศไทยจาก SimpleMaps
-# geojson_url = 'https://simplemaps.com/static/svg/country/th/admin1/th.json'
-# geojson_data = requests.get(geojson_url).json()
-
-# # สร้าง DataFrame ตัวอย่าง
-
-# provinces = [
-#     "Amnat Charoen", "Ang Thong", "Bangkok", "Bueng Kan", "Buriram", "Chachoengsao",
-#     "Chai Nat", "Chaiyaphum", "Chanthaburi", "Chiang Mai", "Chiang Rai", "Chonburi",
-#     "Chumphon", "Kalasin", "Kamphaeng Phet", "Kanchanaburi", "Khon Kaen", "Krabi",
-#     "Lampang", "Lamphun", "Loei", "Lopburi", "Mae Hong Son", "Maha Sarakham",
-#     "Mukdahan", "Nakhon Nayok", "Nakhon Pathom", "Nakhon Phanom", "Nakhon Ratchasima",
-#     "Nakhon Sawan", "Nakhon Si Thammarat", "Nan", "Narathiwat", "Nong Bua Lam Phu",
-#     "Nong Khai", "Nonthaburi", "Pathum Thani", "Pattani", "Phang Nga", "Phatthalung",
-#     "Phayao", "Phetchabun", "Phetchaburi", "Phichit", "Phitsanulok", "Phra Nakhon Si Ayutthaya",
-#     "Phrae", "Phuket", "Prachinburi", "Prachuap Khiri Khan", "Ranong", "Ratchaburi",
-#     "Rayong", "Roi Et", "Sa Kaeo", "Sakon Nakhon", "Samut Prakan", "Samut Sakhon",
-#     "Samut Songkhram", "Saraburi", "Satun", "Si Sa Ket", "Sing Buri", "Songkhla",
-#     "Sukhothai", "Suphan Buri", "Surat Thani", "Surin", "Tak", "Trang", "Trat",
-#     "Ubon Ratchathani", "Udon Thani", "Uthai Thani", "Uttaradit", "Yala", "Yasothon"
-# ]
-
-# data = {
-#     'province': provinces,
-#     'value': [i % 100 for i in range(77)]
-# }
-# df = pd.DataFrame(data)
-
-# # Layout ของแอปพลิเคชัน Dash
-# app.layout = html.Div([
-#     html.H1("Thailand Map", style={'textAlign': 'center'}),
-#     dcc.Graph(id='thailand-map'),
-#     html.Div(id='province-output')
-# ])
-
-# # Callback เพื่ออัปเดตแผนที่และจัดการการคลิกที่จังหวัด
-# @app.callback(
-#     Output('thailand-map', 'figure'),
-#     [Input('thailand-map', 'clickData')]
-# )
-# def update_map(click_data):
-#     fig = px.choropleth_mapbox(
-#         df,
-#         geojson=geojson_data,
-#         locations='province',
-#         featureidkey='properties.name',
-#         color='value',
-#         color_continuous_scale="Viridis",
-#         mapbox_style="carto-positron",
-#         center={"lat": 13.736717, "lon": 100.523186},
-#         zoom=5
-#     )
-#     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-#     if click_data:
-#         selected_province = click_data['points'][0]['location']
-#         fig.update_traces(marker=dict(line=dict(width=2, color='red')))
-#         fig.add_trace(
-#             px.scatter_mapbox(
-#                 df[df['province'] == selected_province],
-#                 lat=[13.736717],
-#                 lon=[100.523186],
-#                 text=[selected_province],
-#                 marker=dict(size=10, color='red'),
-#             ).data[0]
-#         )
-
-#     return fig
-
-# # Callback เพื่ออัปเดตข้อมูลที่แสดงเมื่อมีการคลิกที่จังหวัด
-# @app.callback(
-#     Output('province-output', 'children'),
-#     [Input('thailand-map', 'clickData')]
-# )
-# def display_province_data(click_data):
-#     if click_data:
-#         selected_province = click_data['points'][0]['location']
-#         province_value = df[df['province'] == selected_province]['value'].values[0]
-#         return html.Div([
-#             html.H4(f"Province: {selected_province}"),
-#             html.P(f"Value: {province_value}")
-#         ])
-#     return "Click on a province to see more details."
-
-# # รันแอปพลิเคชัน
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
-
-
+import json
 import dash
 from dash import dcc, html, Input, Output
 import plotly.express as px
@@ -108,9 +9,8 @@ import requests
 # สร้างแอปพลิเคชัน Dash
 app = dash.Dash(__name__)
 
-# โหลดข้อมูล GeoJSON ของประเทศไทยจาก SimpleMaps
-geojson_url = 'https://simplemaps.com/static/svg/country/th/admin1/th.json'
-geojson_data = requests.get(geojson_url).json()
+with open('D:\\1_2567\\241_353\\p_boat\\rongrean\\rongrean\\province.json', 'r', encoding='utf-8') as file:
+    geojson_data = json.load(file)
 
 # สร้าง DataFrame ตัวอย่าง
 provinces = [
@@ -135,11 +35,19 @@ data = {
 }
 df = pd.DataFrame(data)
 
-# Layout ของแอปพลิเคชัน Dash
+# อ่านข้อมูลจากไฟล์ CSV
+school_data = pd.read_csv('D:\\1_2567\\241_353\\p_boat\\rongrean\\rongrean\\graduated.csv')
+# เชื่อมคอลัมน์ 'province' กับ 'school_english'
+merged_df = pd.merge(df, school_data, how='left', left_on='province', right_on='school_english')
+
+# print(merged_df.head())
+
+# Layout ของ Dash
 app.layout = html.Div([
     html.H1("แผนที่ประเทศไทย", style={'textAlign': 'center'}),
     dcc.Graph(id='thailand-map'),
-    html.Div(id='province-output')
+    html.Div(id='province-output'),
+    html.Div(id='school-output')  # ส่วนแสดงข้อมูลโรงเรียน
 ])
 
 # Callback เพื่ออัปเดตแผนที่และจัดการการคลิกที่จังหวัด
@@ -149,62 +57,58 @@ app.layout = html.Div([
 )
 def update_map(click_data):
     fig = px.choropleth_mapbox(
-        df,
+        merged_df,
         geojson=geojson_data,
         locations='province',
         featureidkey='properties.name',
         color='value',
-        color_continuous_scale="Plasma",  # เปลี่ยนสีเป็น Plasma
+        color_continuous_scale="Plasma",
         mapbox_style="carto-positron",
         center={"lat": 13.736717, "lon": 100.523186},
         zoom=5
     )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-    # if click_data:
-    #     selected_province = click_data['points'][0]['location']
-    #     fig.update_traces(marker=dict(line=dict(width=2, color='red')))
-    #     fig.add_trace(
-    #         px.scatter_mapbox(
-    #             df[df['province'] == selected_province],
-    #             lat=[13.736717],
-    #             lon=[100.523186],
-    #             text=[selected_province],
-    #             marker=dict(size=10, color='red'),
-    #         ).data[0]
-    #     )
-
     if click_data:
         selected_province = click_data['points'][0]['location']
-        fig.update_traces(marker=dict(line=dict(width=2, color='red')))
-        fig.add_trace(
-            px.scatter_mapbox(
-                data_frame=df[df['province'] == selected_province],
-                lat=[13.736717],
-                lon=[100.523186],
-                text=[selected_province],
-                customdata=[df[df['province'] == selected_province]['value'].values[0]],
-            ).data[0]
+        fig.update_traces(
+            marker=dict(size=10, color='red'),
+            selector=dict(type='scattermapbox', customdata=merged_df[merged_df['province'] == selected_province]['value'])
         )
-        
+
     return fig
 
-# Callback เพื่ออัปเดตข้อมูลที่แสดงเมื่อมีการคลิกที่จังหวัด
+# Callback 
 @app.callback(
     Output('province-output', 'children'),
+    Output('school-output', 'children'),
     [Input('thailand-map', 'clickData')]
 )
 def display_province_data(click_data):
     if click_data:
         selected_province = click_data['points'][0]['location']
-        province_value = df[df['province'] == selected_province]['value'].values[0]
-        return html.Div([
-            html.H4(f"จังหวัด: {selected_province}"),
-            html.P(f"ค่า: {province_value}")
-        ])
-    return "คลิกที่จังหวัดเพื่อดูข้อมูลเพิ่มเติม."
+        province_data = merged_df[merged_df['province'] == selected_province]
+        if not province_data.empty:
+            total_std = province_data['totalstd'].values[0]
+            total_female = province_data['totalfemale'].values[0]
+            total_male = province_data['totalmale'].values[0]
+            return (
+                html.Div([
+                    html.H4(f"จังหวัด: {selected_province}"),
+                    html.P(f"ค่า: {province_data['value'].values[0]}"),
+                ]),
+                html.Div([
+                    html.H4(f"ข้อมูลโรงเรียนในจังหวัด: {selected_province}"),
+                    html.P(f"นักเรียนทั้งหมด: {total_std}"),
+                    html.P(f"นักเรียนหญิง: {total_female}"),
+                    html.P(f"นักเรียนชาย: {total_male}")
+                ])
+            )
+    return (
+        "คลิกที่จังหวัดเพื่อดูข้อมูลเพิ่มเติม.",
+        ""
+    )
 
 # รันแอปพลิเคชัน
 if __name__ == '__main__':
     app.run_server(debug=True)
-
